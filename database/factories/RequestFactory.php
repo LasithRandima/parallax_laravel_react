ad9ce268a7580460ca8eaf84b09045ca4825171e
Lasith Randima <lasithrandima123@gmail.com>
2024-07-03 16:20:37 +0530

    creating factories/seeders for request model

--- database/factories/RequestFactory.php
+++ database/factories/RequestFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Request;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Request>
@@ -17,7 +18,14 @@ class RequestFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'created_on' => $this->faker->date(),
+            'location' => $this->faker->address(),
+            'service' => $this->faker->word(),
+            'status' => $this->faker->randomElement(['NEW', 'IN_PROGRESS', 'ON_HOLD', 'REJECTED', 'CANCELLED']),
+            'priority' => $this->faker->randomElement(['HIGH', 'MEDIUM', 'LOW']),
+            'department' => $this->faker->word(),
+            'requested_by' => $this->faker->name(),
+            'assigned_to' => $this->faker->name(),
         ];
     }
 }
--- database/seeders/RequestSeeder.php
+++ database/seeders/RequestSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Request;
 
 class RequestSeeder extends Seeder
 {
@@ -12,6 +13,6 @@ class RequestSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        \App\Models\Request::factory(50)->create();
     }
 }
